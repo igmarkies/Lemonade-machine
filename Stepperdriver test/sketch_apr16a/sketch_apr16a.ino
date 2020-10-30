0802778a893d0194e7cdd1b6b04764f97f442942
Ian <ig.markies@gmail.com>
2020-10-30 01:11:00 +0100

    Small update for adding the water pump

--- Stepperdriver test/sketch_apr16a/sketch_apr16a.ino
+++ Stepperdriver test/sketch_apr16a/sketch_apr16a.ino
@@ -1,14 +1,19 @@
 // Define stepper motor connections and steps per revolution:
-#define dirPin 8
-#define stepPin 9
+#define enable 2
+#define dirPin 3
+#define stepPin 4
 #define stepsPerRevolution 200
 
 float step_count = 0;
 float count = 0;
 
-const float mlRotate = 0.665;
+const float mlRotateS = 0.665;   //Sirup pump
+const float mlRotateW = 1.0;       //Water pump
 const float desVolume = 1000.00;
-float desRotate = (desVolume/mlRotate)*200;
+float mlSirup = desVolume/8;
+float sirupRotate = (mlSirup/mlRotateS)*stepsPerRevolution;
+float mlWater = desVolume - mlSirup;
+float waterRotate = (mlWater/mlRotateW)*stepsPerRevolution;
 float compair = 10000;
 void setup() {
   Serial.begin (9600);
@@ -16,13 +21,13 @@ void setup() {
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
   Serial.println ("Hello");
-  Serial.println (desRotate);
+  Serial.println (sirupRotate);
 }
 void loop() {
   
   // Set the spinning direction clockwise:
   digitalWrite(dirPin, LOW);
-  if (step_count <= desRotate){
+  if (step_count <= sirupRotate){
     // These four lines result in 1 step:
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(700);
